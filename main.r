079b011543ceede32924b52255aed852036273e7
Carmeng08 <cgp1018@alu.ubu.es>
2024-10-29 18:46:32 +0100

    modificacion main

--- main.r
+++ main.r
@@ -3,13 +3,20 @@
 ###  CARGA DE PAQUETES:
 install.packages("pxR")
 library(pxR)
-
+install.packages("tidyverse")
+install.packages("json")
+install.packages("jsonlite")
 #CARGA DE DATOS DE CASOS DE PERSONAS QUE TOMAN ALCOHOL HABITUALMENTE POR COMUNIDADES:
 casos_alcohol<-read.px("INPUT/DATA/CASOS_ALCOHOL/CasosAlcoholComunidades.px")
 dtalcohol<-as.data.frame(casos_alcohol)
 str(dtalcohol)
 
 #CARGA DE DATOS DE CASOS DE CANCER EN ESPAÑA POR COMUNIDADES:
+library(tidyverse)
+library(jsonlite)
+library(json)
+library(dplyr)
+library(RJSONIO)
 casos_cancer <- fromJSON(file = "INPUT/DATA/CASOS_CANCER/casos_nuevos_de_cancer.json")
 
 #CARGA DE DATOS DE ZONAS VERDES EN ESPAÑA POR COMUNIDADES:
