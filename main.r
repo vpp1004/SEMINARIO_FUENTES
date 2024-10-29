5820a9f04fb412dad56ddaa6d79d8246d97c49ef
vpp1004 <vpp1004@alu.ubu.es>
2024-10-29 18:31:26 +0100

    Cambios código

--- main.r
+++ main.r
@@ -1,3 +1,21 @@
-# Esto es un script
 
-date() #para obtener la fecha
+
+###  CARGA DE PAQUETES:
+install.packages("pxR")
+library(pxR)
+
+#CARGA DE DATOS DE CASOS DE PERSONAS QUE TOMAN ALCOHOL HABITUALMENTE POR COMUNIDADES:
+casos_alcohol<-read.px("INPUT/DATA/CASOS_ALCOHOL/CasosAlcoholComunidades.px")
+dtalcohol<-as.data.frame(casos_alcohol)
+str(dtalcohol)
+
+#CARGA DE DATOS DE CASOS DE CANCER EN ESPAÑA POR COMUNIDADES:
+casos_cancer <- fromJSON(file = "INPUT/DATA/CASOS_CANCER/casos_nuevos_de_cancer.json")
+
+#CARGA DE DATOS DE ZONAS VERDES EN ESPAÑA POR COMUNIDADES:
+
+#CARGA DE DATOS DE PERSONAS QUE REALIZAN EJERCICIO FISICO HABITUALMENTE POR COMUNIDADES:
+
+
+
+
